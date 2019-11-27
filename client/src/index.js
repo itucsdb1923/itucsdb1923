97dda684aca32dc4c6ea673faa7bdcc256a987fd
ismailak <bergama_itu@hotmail.com>
2019-11-27 17:02:55 +0300

    }}

--- client/src/index.js
+++ client/src/index.js
@@ -4,12 +4,14 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import LoginPage from "./pages/LoginPage";
 import MainPage from "./pages/MainPage";
+import MoviePage from "./pages/MoviePage";
 
 render((
   <Router>
     <Switch>
       <Route exact path="/" component={MainPage} />
       <Route exact path="/login" component={LoginPage} />
+      <Route exact path="/movies" component={MoviePage} />
       <Route exact path="*">404</Route>
     </Switch>
   </Router>
--- client/src/pages/MoviePage.js
+++ client/src/pages/MoviePage.js
@@ -1,7 +1,75 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import MainTemplate from "./MainTemplate";
+import { Accordion, Card, Container, ProgressBar, Button } from 'react-bootstrap';
+//import { useAccordionToggle } from 'react-bootstrap/AccordionToggle';
+
+//const decoratedOnClick = useAccordionToggle(eventKey, onClick);
 
 const MoviePage = (props) => {
-    return <div>{props.match.params.movie_id}</div>
+
+  const [state, setState] = useState({
+    data: []
+  });
+
+  useEffect(() => {
+    let isCancelled = false;
+
+    fetch("/api/movies")
+      .then(res => res.json())
+      .then(data => {
+        if (!isCancelled)
+          setState({ data: data })
+      })
+      .catch((error) => console.error(error));
+
+    return () => { isCancelled = true };
+  }, []);
+
+  const items = state.data.map((item) => {
+    return (
+      <Card key={item.item_id}>
+        <Accordion.Toggle as={Card.Header} eventKey={item.item_id} align="middle">
+          {item.title + " (" + item.year + ")"}
+        </Accordion.Toggle>
+        <Accordion.Collapse eventKey={item.item_id}>
+          <Card.Body>
+            <Card.Title>{item.director}</Card.Title>
+          <Card.Text>
+            {"CAST : " + item.cast}
+            <br />
+            {"DESCRIPTION : " + item.description}
+            <br />
+          </Card.Text>
+          <div align="middle">
+          <div style={{width : "50%"}}>
+          {"Listist Score"}
+          <ProgressBar now={item.score * 10} label={`${item.score + " (" + item.votes + ")"}`} />
+          {"Imdb Score"}
+          <ProgressBar variant="warning"  now={item.imdb_score * 10} label={`${item.imdb_score}`} />
+          </div></div>
+          <br />
+          <div align="middle">
+          <Button variant="primary">ADD</Button>
+          </div>
+          </Card.Body>
+        </Accordion.Collapse>
+      </Card>)
+  })
+
+  return (
+    <MainTemplate>
+      <br />
+      <Container>
+        <Accordion defaultActiveKey="0">
+          {items}
+        </Accordion>
+      </Container>
+    </MainTemplate>
+  )
 }
 
 export default MoviePage;
+
+
+
